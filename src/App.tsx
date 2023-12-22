6463f21ad97001361ac576abc03cfcbdc56f660d
TimLedger <tim_ledger@mail.ru>
2023-12-22 20:27:37 +0600

    создан компонент игральной карты

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import CardDeck from './lib/CardDeck';
+import Card from './Card';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [hand, setHand] = useState<Array<{ rank: string; suit: string }>>([]);
+
+  const dealCards = () => {
+    const deck = new CardDeck();
+    const newHand = deck.getCards(5);
+    setHand(newHand);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={dealCards}>Раздать карты</button>
+      {hand.length > 0 && (
+        <div className="playingCards faceImages">
+          {hand.map((card, index) => (
+            <Card key={index} rank={card.rank} suit={card.suit} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
